47043c252404ed6d29364037df27c64f8c564a39
Koichi Takahashi <tkou15.work@gmail.com>
2022-02-15 02:51:56 +0900

    update template (#1)
    * update
    
    * modify router package

---
+++ lib/ui/routes/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flapp/ui/routes/route_path.dart';
+import 'package:flapp/ui/xxx/xxx_page.dart';
+import 'package:routemaster/routemaster.dart';
+import 'package:flutter/material.dart';
+
+final routes = RouteMap(
+  routes: {RoutePath.root: (_) => const MaterialPage(child: XXXPage())},
+);
